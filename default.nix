e512cb58768c08dc5e388207fdc2cf820c14161f
lukemwaddington <l.waddington@lancaster.ac.uk>
2023-02-22 16:41:57 +0000

    feat: Move autocomplete files to another directory

--- default.nix
+++ default.nix
@@ -17,8 +17,8 @@ stdenv.mkDerivation {
     mkdir -p $out/share
     cp -r ./scripts $out/share/scripts
     mkdir -p $out/share/bash-completion/completions
-    cp ./doctools.autocomplete.bash $out/share/bash-completion/completions/dt
+    cp ./autocomplete/doctools.autocomplete.bash $out/share/bash-completion/completions/dt
     mkdir -p $out/share/fish/vendor_completions.d
-    cp ./doctools.autocomplete.fish $out/share/fish/vendor_completions.d/dt.fish
+    cp ./autocomplete/doctools.autocomplete.fish $out/share/fish/vendor_completions.d/dt.fish
   '';
 }
